0bb4d96a647d2e1c9c14a74ff33d5f4a0b228a7f
Vipansh Thakur <vipansh@github.com>
2021-06-13 00:09:48 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import { FormPage } from './components/FormPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto">
+      <center className="flex justify-items-center items-center">Board Result</center>
+      <FormPage/>
     </div>
   );
 }
---
+++ src/components/FormPage.jsx
@@ -0,0 +1,167 @@
+import React, { useState } from "react";
+
+export const FormPage = () => {
+  const tamplet = {
+    nine:"",
+    firstTerm: "",
+    secondTerm: "",
+    preBoard: "",
+    hindi: "",
+  };
+  const [result, setResult] = useState(tamplet);
+  const [finalResult, setFinalResult] = useState();
+  const [totalMarks, setTotalMarks] = useState(0)
+
+  function changeValue(e) {
+    let value = e.target.value;
+    let name = e.target.name;
+    setResult({ ...result, [name]: value });
+  }
+
+  function percentageof(x, y) {
+    return (x / y) * 100;
+  }
+
+  function xpercentofy(x, y) {
+    return ( x / 100 ) * y;
+  }
+
+  function giveResult() {
+    console.log(result);
+    let ninthMarks = Math.ceil(xpercentofy(10, percentageof(result.nine, 700)));
+
+    if(ninthMarks<33){
+        ninthMarks = xpercentofy(10,33)
+    }
+
+    let firstTermMarks = Math.ceil(
+      xpercentofy(15, percentageof(result.firstTerm, 50))
+    );
+    if(firstTermMarks<33){
+        firstTermMarks=(xpercentofy(15,33))
+    }
+    let secondTermMarks = Math.ceil(
+      xpercentofy(15, percentageof(result.secondTerm, 85))
+    );
+
+    if(secondTermMarks<33){
+        secondTermMarks=(xpercentofy(15,33))
+    }
+
+    let preBoardMarks = Math.ceil(
+      xpercentofy(40, percentageof(result.preBoard, 85))
+    );
+
+    if(preBoardMarks<33){
+        preBoardMarks=(xpercentofy(40,33))
+    }
+    let hindiMarks = Math.ceil(xpercentofy(5, percentageof(result.hindi, 85)));
+    if(hindiMarks<33){
+        hindiMarks=(xpercentofy(5,33))
+    }
+
+    setFinalResult({
+      nine: ninthMarks,
+      firstTerm: firstTermMarks,
+      secondTerm: secondTermMarks,
+      preBoard: preBoardMarks,
+      hindi: hindiMarks,
+    });
+
+    setTotalMarks(ninthMarks+firstTermMarks+ secondTermMarks+preBoardMarks+hindiMarks)
+
+  }
+
+  return (
+    <div>
+      <div class="mt-8 flex">
+        <div class="uppercase w-1/2 text-sm text-gray-600 font-bold">9Th result(weightage 10%) </div>
+        <input
+          class="w-full bg-gray-300 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+          type="number"
+          name="nine"
+          value={result.name}
+          onChange={(e) => {
+            changeValue(e);
+          }}
+        />
+      </div>
+
+      <div class="mt-8 flex">
+        <div class="uppercase w-1/2 text-sm text-gray-600 font-bold">
+          10th First Term 9Th result(weightage 15%)
+        </div>
+        <input
+          class="w-full bg-gray-300 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+          type="number"
+          name="firstTerm"
+          value={result.firstTerm}
+          onChange={(e) => {
+            changeValue(e);
+          }}
+        />
+      </div>
+
+      <div class="mt-8 flex">
+        <div class="uppercase w-1/2 text-sm text-gray-600 font-bold">
+          10Th second term 9Th result(weightage 15%)
+        </div>
+        <input
+          class="w-full bg-gray-300 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+          type="number"
+          name="secondTerm"
+          value={result.secondTerm}
+          onChange={(e) => {
+            changeValue(e);
+          }}
+        />
+      </div>
+
+      <div class="mt-8 flex">
+        <div class="uppercase w-1/2 text-sm text-gray-600 font-bold">pre Board 9Th result(weightage 40%)</div>
+        <input
+          class="w-full bg-gray-300 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+          type="number"
+          name="preBoard"
+          value={result.preBoard}
+          onChange={(e) => {
+            changeValue(e);
+          }}
+        />
+      </div>
+
+      <div class="mt-8 flex">
+        <div class="uppercase w-1/2 text-sm text-gray-600 font-bold">Hindi exam 9Th result(weightage 5%)</div>
+        <input
+          class="w-full bg-gray-300 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+          type="number"
+          name="hindi"
+          value={result.hindi}
+          onChange={(e) => {
+            changeValue(e);
+          }}
+        />
+      </div>
+
+      <button className="m-4 p-4 border rounded-md" onClick={giveResult}>Calculate</button>
+      <button className="m-4 p-4 border rounded-md" onClick={()=>{
+          setFinalResult(tamplet)
+          setResult(tamplet)
+      }}>Reset</button>
+
+
+      <div>Result</div>
+      {finalResult&&<div>
+        <div> ninth: {finalResult.nine}</div>
+    <div>  first Term: {finalResult.firstTerm}</div>
+    <div>  second Term: {finalResult.secondTerm}</div>
+    <div>  pre Board: {finalResult.preBoard}</div>
+    <div>  hindi: {finalResult.hindi}</div>
+    <div>Total: {Math.ceil(totalMarks)}</div>
+          </div>}
+     
+
+
+    </div>
+  );
+};
